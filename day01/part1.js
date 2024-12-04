44904b9c8fc5236b3618e9add47156afa9e44bf4
Patrik Gaicean <patrik@nassautech.xyz>
2024-12-04 21:21:34 +0200

    day01 - part1

---
+++ day01/part1.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs/promises';
+
+async function main() {
+    const file = await fs.open('./input');
+
+    let left = [];
+    let right = [];
+    for await (const line of file.readLines()) {
+        const pair = line.split('   ').map(a => Number(a));
+        left.push(pair[0]);
+        right.push(pair[1]);
+    }
+
+    const compareFunc = (a, b) => a - b;
+    left.sort(compareFunc);
+    right.sort(compareFunc);
+
+    let sum = 0;
+    for (let i = 0; i < left.length; i++) {
+        const l = left[i];
+        const r = right[i];
+
+        sum += l >= r ? l - r : r - l;
+    }
+
+    console.log(sum);
+}
+
+main();
+
